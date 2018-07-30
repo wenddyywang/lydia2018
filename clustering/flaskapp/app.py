1b2b3099e80a1d757c72c65451c40d928adfe4ea
Wendy Wang <www2105@columbia.edu>
2018-07-30 13:19:52 -0400

    reclustering button

--- clustering/flaskapp/app.py
+++ clustering/flaskapp/app.py
@@ -1,9 +1,17 @@
 from flask import Flask, render_template
+import json_coms_categorizer
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+	print("index")
+	return render_template('coms_clusters.html')
+
+@app.route('/recluster/', methods=['POST'])
+def recluster():
+	print("reclustering")
+	json_coms_categorizer.run()
 	return render_template('coms_clusters.html')
 
 if __name__ == '__main__':
